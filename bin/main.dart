d955a4af2ad896ed4a51541998b2e4dd2e174f43
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-03-22 01:28:38 +0530

    print hello world

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,3 @@
-import 'package:learning_dart/learning_dart.dart' as learning_dart;
-
 main(List<String> arguments) {
-  print('Hello world: ${learning_dart.calculate()}!');
+  print("Hello world");
 }
