888ae9299777b2fefd824dba242f2a49b1352a74
Rohan Taneja <rohan.bvcoe@gmail.com>
2018-03-22 17:05:18 +0530

    shorthand syntax for functions

--- bin/main.dart
+++ bin/main.dart
@@ -2,6 +2,7 @@ main(List<String> arguments) {
   print(isGreater(10, 20));
 }
 
-isGreater(int a, int b) {
-  return a > b;
-}
+//function(...) => expression;
+// is an alternative to
+//function(...){ return expression;}
+isGreater(int a, int b) => a > b;
